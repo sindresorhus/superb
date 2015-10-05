4ea031ece609b728570ba444dd8e131d6d138792
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-05 21:05:10 +0700

    bump AVA
    https://github.com/sindresorhus/ava

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
-'use strict';
-var test = require('ava');
-var superb = require('./');
+import test from 'ava';
+import fn from './';
 
-test('returns a random superb word', function (t) {
-	t.assert(superb().length > 0);
-});
-
-test('returns all the words', function (t) {
-	t.assert(Array.isArray(superb.words));
-	t.assert(superb.words[3].length > 0);
+test(t => {
+	t.true(fn().length > 0);
+	t.true(Array.isArray(fn.words));
+	t.true(fn.words[3].length > 0);
+	t.end();
 });
