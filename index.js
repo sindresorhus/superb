4336297151daaf3b4b93a273cf8efcefa21ba9cc
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-26 13:43:10 +0200

    fix XO lint issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
-var words = require('./words.json');
 var uniqueRandomArray = require('unique-random-array');
+var words = require('./words.json');
 
 module.exports = uniqueRandomArray(words);
 module.exports.words = words;
